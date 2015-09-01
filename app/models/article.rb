fc8d07e235495afb71238df05913a21acf0a63ec
Tyler Machen <tylermachen@gmail.com>
2015-09-01 11:19:13 -0400

    add limit and update article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,34 +1,33 @@
 class Article < ActiveRecord::Base
   require 'json'
   require 'open-uri'
-  require 'pry'
 
   BASE_URL = "https://www.reddit.com/r"
+  LIMIT = 10
 
   def get_url(category)
-    "#{BASE_URL}/#{category}/.json"
+    "#{BASE_URL}/#{category}.json?limit=#{LIMIT}"
   end
 
   def get_json(url)
     JSON.load(open(url))
   end
 
-  def get_article_info(article_hash)
-    article_hash["data"]["children"].each do |a|
-      title = a["data"]["title"]
-      rank = a["data"]["ups"]
-      link = a["data"]["url"]
-      comment_link = a["data"]["permalink"]
-      num_comments = a["data"]["num_comments"]
-      domain = a["data"]["domain"]
-      created_utc = (Time.at(a["data"]["created"])).strftime("Posted " +
-                     "on %A %-m/%d/%Y at %-l:%M%P")
-    end
-  end
+  # def get_article_info(article_hash)
+  #   article_hash["data"]["children"].each do |a|
+  #     title = a["data"]["title"]
+  #     rank = a["data"]["ups"]
+  #     link = a["data"]["url"]
+  #     comment_link = a["data"]["permalink"]
+  #     num_comments = a["data"]["num_comments"]
+  #     domain = a["data"]["domain"]
+  #     created_utc = a["data"]["created"]
+  #   end
+  # end
 
   def article(category)
     url = get_url(category)
     hash = get_json(url)
-    get_article_info(hash)
+    # get_article_info(hash)
   end
 end
