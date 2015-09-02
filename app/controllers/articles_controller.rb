c7fd3f809f333b7f61a666b48318402fa6c6ab8d
Tyler Machen <tylermachen@gmail.com>
2015-09-01 22:04:32 -0400

    cleanup controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,11 @@
 class ArticlesController < ApplicationController
   include ArticleParser
+
   def index
     category = 'news'
     url = get_url(category)
     hash = get_json(url)
     get_article_info(hash)
     @articles = Article.all
-  end
-
-  def create
-    render nothing: true
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    render nothing: true
   end
 end
