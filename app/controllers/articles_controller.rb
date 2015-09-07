2bd3a7c4ca5d936713761ae859fa7be2d2d982a0
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-07 16:53:57 -0400

    fix bug where user can submit without selecting a category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,24 @@ class ArticlesController < ApplicationController
   respond_to :html, :js
 
   def index
-    @categories = Category.all
-    category = params[:category][:id]
-    category_id = Category.where(name: category).first.id
-    url = get_url(category)
-    hash = get_json(url)
-    get_article_info(hash, category_id)
-    @articles = Article.all.where("created_at >= ? AND category_id = ?",
-                Time.zone.now.beginning_of_day, category_id)
+    if valid?
+      @categories = Category.all
+      category = params[:category][:id]
+      category_id = Category.where(name: category).first.id
+      url = get_url(category)
+      hash = get_json(url)
+      get_article_info(hash, category_id)
+      @articles = Article.all.where("created_at >= ? AND category_id = ?",
+                  Time.zone.now.beginning_of_day, category_id)
+    else
+      flash[:notice] = "Please choose a category"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def valid?
+    !category = params[:category][:id].empty?
   end
 end
